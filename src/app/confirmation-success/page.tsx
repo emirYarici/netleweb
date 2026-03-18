9b1f610e5bc116b5613dcba50df66bee985ff431
Emir Yarıcı <emiryarici@EMIRYARICI-MB.local>
2026-03-18 17:10:19 +0300

    info

--- src/app/confirmation-success/page.tsx
+++ src/app/confirmation-success/page.tsx
@@ -7,8 +7,8 @@ export default function ComfirmationSuccessPage() {
             İşleminiz başarılı oldu!
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            İşlemi tamamladığınız için teşekkür ederiz. Artık bir sonraki
-            adımlarınıza geçebilirsiniz.
+            İşlemi tamamladığınız için teşekkür ederiz. Artık Uygulama üzerinden
+            giriş yapmayı deneyebilirsiniz
           </p>
         </div>
       </main>
